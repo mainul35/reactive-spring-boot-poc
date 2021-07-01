713988c1ec394c3530ded3b77970979865b7de59
mainul35 <mainuls18@gmail.com>
2021-07-02 00:05:56 +0700

    rendering hello thymeleaf view page from reactive controller

--- projects/reactive-webmvc-with-thymeleaf/src/main/java/com/mainul35/controller/HelloController.java
+++ projects/reactive-webmvc-with-thymeleaf/src/main/java/com/mainul35/controller/HelloController.java
@@ -2,23 +2,23 @@ package com.mainul35.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
 
-@RestController
+@Controller
 public class HelloController {
 	
 	Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 	
 	@GetMapping({"", "/"})
-	public Mono<String> hello() {
-		LOGGER.info("======== Printing an INFO Log ========");
-		LOGGER.debug("======== Printing an DEBUG Log ========");
-		LOGGER.warn("======== Printing an WARNING Log ========");
-		LOGGER.error("======== Printing an Error Log ========");
-        return Mono.just("Hello world.");
+	public Mono<String> hello(final Model model) {
+		LOGGER.info("======== In Hello controller ========");
+		model.addAttribute("message", "Hello from reactive controller");
+        return Mono.just("hello");
     }
 
 }
